f24b22cf6687feece31b76e832422abf4c74fcd2
Daniel Noble <daniel.wa.noble@gmail.com>
2017-07-13 08:45:40 +1000

    Added updated simulation parameters to make the data look a little nicer

--- R/sim.R
+++ R/sim.R
@@ -7,25 +7,26 @@
 ##################################################################
 
 # Parameters for simulations
-	k = 40 			#Number of individuals 
-	n = 30 			#Number of trials or repeated measurements for each individual 
-	trtE = 0.3  	#Treatment effect we will model
-	trialE = 0.4 	#Often there are very strong trial effects, so we will include this
-	IntE = 0.3 		# Often we will test for an interactions between trial and treatment as this would imply different rates of learning.
-	bo = 0.05		# Individual intercepts
+	     k = 40 	# Number of individuals 
+	     n = 30     # Number of trials or repeated measurements for each individual 
+	  trtE = 2  	# Treatment effect we will model
+	trialE = 0.4 	# Often there are very strong trial effects, so we will include this
+	  IntE = 0.3 	# Interaction coefficient
+	    bo = -8		# Individual intercepts
 
 # Create some predictors we need
-	trt <- rep(c(1,0), each = (k*n)/2)
+	  trt <- rep(c(1,0), each = (k*n)/2)
 	trial <- rep(1:30, k)
 	indVI <- rep(rnorm(k, 0, sqrt(0.64)), each = n)
 	indSL <- rep(rnorm(k, 0, sqrt(0.25)), each = n)
-	ind <- as.character(rep(1:k, each = n))
+	  ind <- as.character(rep(1:k, each = n))
 
-# Simulate some choice data 
-	
+# Generate the probabilities based on the predictors 
 	prob <- exp((bo) + (trtE)*trt + trialE*trial) / (1 + exp((bo) + (trtE)*trt + trialE*trial))
 
-# Have a look at the data
+# Have a look at the 'true' data
 	data <- data.frame(ind, trial, trt, prob)
+	lattice::xyplot(prob ~ trial | trt, data = data)
 
-	choices <- rbinom(k*n, 1, )
+# Simulate some choice data based on the probabilities for each observation.
+	choices <- rbinom(k*n, 1, prob)
